ebdec653c5e02986b1dcd155f1e9fa8645207abf
Fabricio-Guima <fsgkof@gmail.com>
2024-09-21 20:40:10 -0300

    YesNoModel for serialization

---
+++ lib/infrastructure/models/yes_no_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:yes_no_app/prensetation/domain/entities/message.dart';
+
+class YesNoModel {
+  String answer;
+  bool forced;
+  String image;
+
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
+
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+        text: answer == 'yes' ? 'Sim' : 'Não',
+        fromWho: FromWho.hers,
+        imageUrl: image,
+      );
+}
