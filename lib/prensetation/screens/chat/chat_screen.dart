8b7a669189860af926e7d2f30f8e921d79a75b13
Fabricio-Guima <fsgkof@gmail.com>
2024-09-21 17:09:39 -0300

    send message to chatProvider

--- lib/prensetation/screens/chat/chat_screen.dart
+++ lib/prensetation/screens/chat/chat_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/main.dart';
+import 'package:yes_no_app/prensetation/domain/entities/message.dart';
+import 'package:yes_no_app/prensetation/providers/chat_provider.dart';
 import 'package:yes_no_app/prensetation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/prensetation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/prensetation/widgets/shared/message_field_box.dart';
@@ -14,7 +18,7 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(),
         ),
-        title: const Text('Amore <3'),
+        title: const Text('Contact'),
         centerTitle: true,
       ),
       body: _ChatView(),
@@ -25,6 +29,8 @@ class ChatScreen extends StatelessWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -32,14 +38,18 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-                    itemCount: 100,
+                    itemCount: chatProvider.messageList.length,
                     itemBuilder: (context, index) {
-                      return (index % 2 == 0)
-                          ? const HerMessagebubble()
-                          : const MyMessagebubble();
+                      final message = chatProvider.messageList[index];
+
+                      return (message.fromWho == FromWho.me)
+                          ? MyMessagebubble(message: message)
+                          : const HerMessagebubble();
                     })),
             // teclado para digitar
-            const MessageFieldBox(),
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
           ],
         ),
       ),
