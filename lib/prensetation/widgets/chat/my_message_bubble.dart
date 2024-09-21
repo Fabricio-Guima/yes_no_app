c6a74bf18418934d2f366900f8f01b12f68d909b
Fabricio-Guima <fsgkof@gmail.com>
2024-09-21 17:10:38 -0300

    add style on my_message_bubble

--- lib/prensetation/widgets/chat/my_message_bubble.dart
+++ lib/prensetation/widgets/chat/my_message_bubble.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/prensetation/domain/entities/message.dart';
 
 class MyMessagebubble extends StatelessWidget {
-  const MyMessagebubble({super.key});
+  final Message message;
+  const MyMessagebubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +14,10 @@ class MyMessagebubble extends StatelessWidget {
         Container(
             decoration: BoxDecoration(
                 color: colors.primary, borderRadius: BorderRadius.circular(20)),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Text('teste aqui teste aqui',
-                  style: TextStyle(color: Colors.white)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(message.text,
+                  style: const TextStyle(color: Colors.white)),
             )),
         const SizedBox(height: 10),
       ],
